e5990aa9f660c63629af33035e19fcfcdc4e6f80
Uchauhan784 <Uchauhan784@gmail.com>
2024-12-20 13:41:48 +0530

    card

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,11 +1,10 @@
-
-
 import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './C_Navbar/Navbar'
-import { HomePage } from './Components/Home';
 import { AddEmp } from './Components/AddEmp';
 import { UserFeedback } from './Components/UserFeedback';
+import  CardHome from "./card/card";
 import Footer1 from './footer1/Footer1';
+
 
 function App() {
 
@@ -15,11 +14,13 @@ function App() {
   <Router>
     <Navbar/>
     <Routes>
-      <Route path='/' element={<HomePage/>} />
+      <Route path='/' element={<CardHome/>} />
       <Route path='/AddEmp' element={<AddEmp/>} />
       <Route path='/UserFeedback' element={<UserFeedback/>}/>
     </Routes>
+    
   </Router>
+  
     <Footer1/>
     </>
   )
